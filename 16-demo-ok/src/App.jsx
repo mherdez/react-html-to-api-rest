62dd638f6d2a41983acdea1fceb73029f91f7e32
Manuel Herdez <herdez@gmail.com>
2022-06-14 22:58:51 -0500

    16-demo-ok, 17 y 18

--- 16-demo-ok/src/App.jsx
+++ 16-demo-ok/src/App.jsx
@@ -6,40 +6,36 @@ import Title from './components/Title';
 import UsersList from './components/UsersList';
 
 function App() {
-  const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState([]);
 
-  const newUser = async () => {
-    const respuesta = await fetch(
-      'https://randomuser.me/api/?gender=female&results=9'
-    );
-    const { results } = await respuesta.json();
-    const getUser = results.map((user) => ({ ...user, activo: true }));
-    setUsers([getUser, ...users]);
-  };
+	const newUser = async () => {
+		const respuesta = await fetch(
+			'https://randomuser.me/api/?gender=female&results=9'
+		);
+		const { results } = await respuesta.json();
+		const getUsers = results.map((user) => ({ ...user, activo: true }));
+		setUsers([...getUsers, ...users]);
+	};
 
-  const cambiarActivo = (id) => {
-    const newUsers = [...users];
-    const userChange = newUsers.find((user) => user.login.uuid === id);
-    userChange.activo = !userChange.activo;
-    setUsers(newUsers);
-  };
+	const cambiarActivo = (id) => {
+		const newUsers = [...users];
+		const userChange = newUsers.find((user) => user.login.uuid === id);
+		userChange.activo = !userChange.activo;
+		setUsers(newUsers);
+	};
 
-  const deleteUsers = () => {
-    setUsers(users.filter((user) => user.activo));
-  };
+	useEffect(() => {
+		newUser();
+	}, []);
 
-  useEffect(() => {
-    newUser();
-  }, []);
-
-  return (
-    <>
-      <Title newUser={newUser} deleteUsers={deleteUsers} />
-      <div className='container'>
-        <UsersList users={users} cambiarActivo={cambiarActivo} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Title />
+			<div className='container'>
+				<UsersList users={users} cambiarActivo={cambiarActivo} />
+			</div>
+		</>
+	);
 }
 
 export default App;
--- 16-demo-ok/src/components/UsersList.jsx
+++ 16-demo-ok/src/components/UsersList.jsx
@@ -1,24 +1,22 @@
 import Card from './Card';
 
 const UsersList = ({ users, cambiarActivo }) => {
-  return (
-    <>
-      {users.map((user) => {
-        console.log(user);
-        const { name, login, picture, activo } = user;
-        return (
-          <Card
-            key={login.uuid}
-            id={login.uuid}
-            name={`${name.first} ${name.last}`}
-            photo={picture.large}
-            activo={activo}
-            cambiarActivo={cambiarActivo}
-          />
-        );
-      })}
-    </>
-  );
+	return (
+		<>
+			{users.map(({ name, login, picture, activo }) => {
+				return (
+					<Card
+						key={login.uuid}
+						id={login.uuid}
+						name={`${name.first} ${name.last}`}
+						photo={picture.large}
+						activo={activo}
+						cambiarActivo={cambiarActivo}
+					/>
+				);
+			})}
+		</>
+	);
 };
 
 export default UsersList;
